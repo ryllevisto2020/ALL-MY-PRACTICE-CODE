e362952a772c7f25b8fb175cef3c161a59d15175
ryllevisto2020 <ryllevisto13@gmail.com>
2023-11-13 11:04:48 +0800

    New file and Modified

--- JAVASCRIPT/project1/index.js
+++ JAVASCRIPT/project1/index.js
@@ -1,9 +1,18 @@
-let arr = [1,2,3,4];
-//Array Push
-arr.push(5);
-console.log(arr);
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const body_parser = require('body-parser');
 
-//Slice
-console.log(arr.slice(1));
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-//
+app.post('/',multer().single('text'),(req,res)=>{
+    console.log(req.text)
+    //res.json(req)
+})
+
+app.listen(3020,()=>{
+    console.log("running on port 3020");
+})
