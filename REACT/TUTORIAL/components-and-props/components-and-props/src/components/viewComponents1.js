d7b8194e49b9d2bfb2f164097b3908fdb45e594f
ryllevisto2020 <ryllevisto13@gmail.com>
2023-11-13 16:13:59 +0800

    New File

---
+++ REACT/TUTORIAL/components-and-props/components-and-props/src/components/viewComponents1.js
@@ -0,0 +1,9 @@
+function ViewComponents() {
+    return (
+      <>
+        <h1>this is components</h1>
+      </>
+    );
+  }
+
+export default ViewComponents;
---
+++ REACT/TUTORIAL/components-and-props/components-and-props/src/components/viewProps.js
@@ -0,0 +1,11 @@
+import React from "react";
+function ViewProps(props) {
+    console.log(props);
+    return (
+        <>
+        <h1>this is { props.name } | Age: {props.age}</h1>
+        </>
+    );
+  }
+
+export default ViewProps;
