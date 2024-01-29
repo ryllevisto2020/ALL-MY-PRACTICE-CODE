a1db3e76a7ec7fb0acd244fc669320f6814cb584
ryllevisto2020 <ryllevisto13@gmail.com>
2024-01-29 10:47:57 +0800

    Load

--- EXPRESS-JS/TUTORIAL/TEMPORARY DOCUMENTATION/REQUEST/index.js
+++ EXPRESS-JS/TUTORIAL/TEMPORARY DOCUMENTATION/REQUEST/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.get('/request/access', (req, res)=>{
+app.get('/request/access/:params', (req, res)=>{
     //Accessing Request Properties
 
     //Load all Request Properties
@@ -17,6 +17,11 @@ app.get('/request/access', (req, res)=>{
     console.log("THIS IS A QUERY PROPERTIES")
     console.log(req.query);
 
+    //Accessing Request Params
+    //URI sample -> /request/access/visto -> localhost:3080/request/access/visto
+    console.log("THIS IS A PARAM PROPERTIES")
+    console.log(req.params);
+
     //Accessing Request Headers
     console.log("THIS IS A HEADERS PROPERTIES")
     console.log(req.headers);
@@ -24,6 +29,10 @@ app.get('/request/access', (req, res)=>{
     //Accessing Request Body Raw Data & X-WWW-FORM-URLENCODED
     console.log("THIS IS A BODY PROPERTIES")
     console.log(req.body);
+
+    //Accessing Request Cookie
+    console.log("THIS IS A COOKIE PROPERTIES")
+    console.log(req.cookies)
 
     res.send("This is a Test!");
 });
